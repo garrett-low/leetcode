c50ef6d3124bf632d5df804a0ce6c8ab356ae41b
Garrett R Low <usernamegl@live.com>
2023-06-03 00:02:07 -0500

    rb2 and moving files again

--- python/fem_algos/array_ring_buffer/array_ring_buffer2.py
+++ python/fem_algos/array_ring_buffer/array_ring_buffer2.py
@@ -26,15 +26,54 @@ class RingBuffer:
         self.length += 1
     
     def peek_tail(self):
+        if self.length <= 0:
+            print("RB:\t\tI'm empty!")
+            return
         index = (self.head + self.length - 1) % self.capacity
         print(f"RB tail:\t[{self.array[index]}]")
     
     def remove_tail(self):
-        pass
+        if self.length <= 0:
+            print("RB:\t\tI'm empty!")
+            return None
+        
+        index = (self.head + self.length - 1) % self.capacity
+        val = self.array[index]
+        self.array[index] = None
+        self.length -= 1
+        return val
+    
+    def add_head(self, val):
+        if self.length >= self.capacity:
+            print("RB:\t\tI'm full!")
+            return
+        
+        self.head = (self.head - 1) % self.capacity
+        index = (self.head) % self.capacity
+        self.array[index] = val
+        self.length += 1
+        
+    def peek_head(self):
+        if self.length <= 0:
+            print("RB:\t\tI'm empty!")
+            return
+        print(self.array[self.head])
+    
+    def remove_head(self):
+        if self.length <= 0:
+            print("RB:\t\tI'm empty!")
+            return None
+        val = self.array[self.head]
+        self.array[self.head] = None
+        self.head = (self.head + 1) % self.capacity
+        self.length -= 1
+        return val
 
 def main():
     buf = RingBuffer(5)
     print(buf)
+    buf.peek_tail()
+    buf.remove_tail()
     buf.add_tail(1)
     buf.peek_tail()
     print(buf)
@@ -47,6 +86,25 @@ def main():
     buf.add_tail(4)
     buf.peek_tail()
     print(buf)
+    buf.remove_tail()
+    print(buf)
+    buf.add_head(0)
+    buf.add_head(-1)
+    print(buf)
+    buf.add_head("fails")
+    buf.remove_tail()
+    buf.remove_head()
+    print(buf)
+    print("\n\nNew test:")
+    buf2 = RingBuffer(69)
+    for i in range(31):
+        buf2.add_tail(i)
+    print(buf2)
+    for i in range(10,421):
+        buf2.add_tail(i)
+        buf2.remove_head()
+        if i % 10 == 0:
+            print(buf2)
     
 if __name__ == "__main__":
     main()
