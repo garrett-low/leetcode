b4593c7eef0b4fcd103d1cf2736a9cefaf827342
Garrett R Low <usernamegl@live.com>
2023-06-03 00:10:16 -0500

    twosum2

---
+++ !by_ds_or_algo/array/TwoSum_leetcode_2.py
@@ -0,0 +1,15 @@
+def twosum(array, target):
+    comp_set = set()
+    for i in range(len(array) - 1):
+        val = array[i]
+        complement = target - array[i]
+        if complement in comp_set:
+            return True
+        comp_set.add(val)
+
+def main():
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(twosum(arr, 5))
+
+if __name__ == "__main__":
+    main()
