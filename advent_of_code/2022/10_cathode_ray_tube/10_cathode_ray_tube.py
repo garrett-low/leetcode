32a98414a1b695b95b8eee0ac928fc395d284491
Garrett R Low <usernamegl@live.com>
2023-06-15 09:32:11 -0500

    aoc day10 p2 start

--- advent_of_code/2022/10_cathode_ray_tube/10_cathode_ray_tube.py
+++ advent_of_code/2022/10_cathode_ray_tube/10_cathode_ray_tube.py
@@ -25,6 +25,37 @@ def sum_signal(filename):
     for cycle, register in target_register.items():
         sum_register += cycle * register
     print(sum_register)
-        
+
+# part two
+def crt(filename):
+    register = 0
+    cycle = 0
+    with open(filename, 'rt', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+#             print(f"{cycle}: {line} ({register})")
+            split = line.split(' ')
+            op = split[0]
+            if op == 'addx':
+                # pre
+                print_crt(register, cycle)
+                cycle += 1
+                print_crt(register, cycle)
+                cycle += 1
+                register += int(split[1])
+            else:
+                print_crt(register, cycle)
+                cycle += 1
+
+def print_crt(register, cycle):
+    mod_cycle = cycle % 40
+    if mod_cycle == 0:
+        print()
+    if register >= cycle - 1 and register <= cycle + 1:
+        print(chr(9608), end='')
+    else:
+        print(chr(9617), end='')
+
 sum_signal("sample.txt")
 sum_signal("input.txt")
+crt("sample.txt")
