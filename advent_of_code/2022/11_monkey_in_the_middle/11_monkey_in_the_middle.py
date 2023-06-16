d5570039a5866df6b4127a387d042da4b9d36e0d
Garrett R Low <usernamegl@live.com>
2023-06-15 21:13:41 -0500

    aoc day11 p1 start

---
+++ advent_of_code/2022/11_monkey_in_the_middle/11_monkey_in_the_middle.py
@@ -0,0 +1,9 @@
+class Monkey:
+    def __init__(self, starting_items, inspect_fn, test_fn, true_monkey, false_monkey):
+        self.starting_items = starting_items
+        self.inspect_fn
+
+def monkey_business(filename):
+    with open(filename, 'rt', encoding='utf-8') as file:
+        for line in file:
+            pass
