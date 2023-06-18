a8607ad9e4e6de370281ec020e1e789cfc860b02
garrett-low <usernamegl@live.com>
2023-06-18 17:54:09 -0500

    DFS on adjacency list - more cleanup

--- python/fem_algos/graph/dfs_adj_list_unweighted.py
+++ python/fem_algos/graph/dfs_adj_list_unweighted.py
@@ -3,28 +3,28 @@ from adj_list_unweighted import adj_list
 # return the DFS path to the node
 # else return empty list
 def dfs(graph, val):
+    path = []
     seen = set()
-    path = ['A'] # let's just say A is starting node
-    curr = 'A'
+    curr = 'A' # let's just say A is starting node
     is_found = dfs_inner(graph, curr, val, path, seen)
     if is_found:
         return path
     return []
 
 def dfs_inner(graph, curr, val, path, seen):
-    if curr == val:
-        return True
     if curr in seen:
         return False
     
     seen.add(curr)
+    path.append(curr)
+    if curr == val:
+        return True
     
     for neighbor in graph.adj_list[curr]:
-        path.append(neighbor)
         if dfs_inner(graph, neighbor, val, path, seen):
             return True
-        path.pop()
     
+    path.pop()
     return False
 
 def main():
