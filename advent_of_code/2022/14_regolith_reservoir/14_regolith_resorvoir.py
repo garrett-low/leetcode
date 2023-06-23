1e9cc3de3079cfe459829776fcd22a5fbe9b50ec
glow <glow@epic.com>
2023-06-23 08:33:59 -0500

    aoc 2022 day 14 p1 draw rock

--- advent_of_code/2022/14_regolith_reservoir/14_regolith_resorvoir.py
+++ advent_of_code/2022/14_regolith_reservoir/14_regolith_resorvoir.py
@@ -11,6 +11,7 @@ class point:
     def __str__(self):
         return f"[{self.x}, {self.y}]"
 
+# make rock_map one unit bigger on left and right
 class rock_map:
     def __init__(self, rock_lines, min_row, max_row, min_col, max_col):
         self.rock_lines = rock_lines
@@ -18,19 +19,55 @@ class rock_map:
         self.max_row = max_row
         self.min_col = min_col
         self.max_col = max_col
+        # empty grid
+        self.width = self.max_col - self.min_col + 2
+        self.height = self.max_row + 5
+        self.grid = [[air for col in range(self.width)] for row in range(self.max_row + 5)]
         #
-        self.height = self.max_col - self.min_col + 1
-        self.width = self.max_row - self.min_row + 1
-        self.grid = [[air for col in range(self.width)] for row in range(self.height)]
+        for rock_line in rock_lines:
+            for i in range(len(rock_line) - 1):
+                self.draw_rock(rock_line[i], rock_line[i + 1])      
+        
     def __str__(self):
         retval = f"{self.min_row}, {self.max_row}, {self.min_col}, {self.max_col}\n"
+        retval += " "
+        for i in range(self.width):
+            retval += f"{i % 9}"
+        retval += "\n"
+        i = 0
         for row in self.grid:
+            retval += f"{i % 9}"
             for col in row:
                 retval += chr(col)
             retval += "\n"
+            i += 1
         return retval
+    
+    def draw_rock(self, point1, point2):
+        if point1.x == point2.x:
+            if point1.y < point2.y:
+                row_start = point1.y
+                row_end = point2.y
+            else:
+                row_start = point2.y
+                row_end = point2.y
+            # inclusive of end point
+            for row in range(row_start, row_end + 1):
+                self.grid[row][self.getx(point1.x)] = rock
+        else:
+            if point1.x < point2.x:
+                col_start = point1.x
+                col_end = point2.x
+            else:
+                col_start = point2.x
+                col_end = point1.x
+            # inclusive of end point
+            for col in range(col_start, col_end + 1):
+                self.grid[point1.y][self.getx(col)] = rock
+    
+    def getx(self, x):
+        return x - self.min_col + 1
 
-# to make coordinates easier, map is flipped so sand falls up.
 def falling_sand(filename):    
     min_row = sys.maxsize
     min_col = sys.maxsize
@@ -45,9 +82,9 @@ def falling_sand(filename):
             # Find min and max row so I don't have to draw an arbitrarily large grid. Yes this means two passes
             for point_str in line_split:
                 point_split = point_str.split(',')
-                p_row = int(point_split[0])
-                p_col = int(point_split[1])
-                new_point = point(p_row, p_col)
+                p_col = int(point_split[0])
+                p_row = int(point_split[1])
+                new_point = point(p_col, p_row)
                 rock_lines[rock_line_idx].append(new_point)
                 if p_row < min_row:
                     min_row = p_row
@@ -58,30 +95,13 @@ def falling_sand(filename):
                 elif p_col > max_col:
                     max_col = p_col
             rock_line_idx += 1  
-    print(min_row)
-    print(max_row)
-    print(min_col)
-    print(max_col)
+#     print(min_row)
+#     print(max_row)
+#     print(min_col)
+#     print(max_col)
     
-    # make rock_map one unit bigger on all sides (?)
-    height = max_col - min_col + 1
-    width = max_row - min_row + 1
-    rockmap = [[air for col in range(width)] for row in range(height)]
-    print_test(rockmap)
     rockmapobj = rock_map(rock_lines, min_row, max_row, min_col, max_col)
     print(rockmapobj)
-    
-    for rock_line in rock_lines:
-        for i in range(len(rock_line) - 1):
-            draw_rock(rock_line[i], rock_line[i + 1], rockmap)
-    
-    print_test(rockmap)
-
-def draw_rock(point1, point2, rockmap):
-    print(f"{point1} -> {point2}")
-    for row in range(point1.y, point2.y):
-        for col in range(point1.x, point2.x):
-            rockmap[row][col] = rock
 
 def print_test(arr):
 #     arr = [[air for col in range(10)] for row in range(10)]
