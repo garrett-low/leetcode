9589009968c825561d65d455296cb8c2542d2639
Garrett R Low <usernamegl@live.com>
2023-07-04 17:19:33 -0500

    aoc 2021 day 1 cleanup

--- advent_of_code/2021/1_sonar_sweep/1_sonar_sweep_part_two.py
+++ advent_of_code/2021/1_sonar_sweep/1_sonar_sweep_part_two.py
@@ -4,7 +4,8 @@ import sys
 def sweep(filename):
     with open(filename, 'rt', encoding='utf-8') as file:
         window = deque()
-        window_sums = []
+        window_sums = deque()
+        num_increase = 0
         for line in file:
             curr = int(line)
             window.append(curr)
@@ -14,15 +15,21 @@ def sweep(filename):
                     threesum += num
                 window_sums.append(threesum)
                 window.popleft()
+            if len(window_sums) == 2:
+                prev = window_sums.popleft()
+                curr = window_sums[0]
+#                 print(f"prev: {prev}, curr: {curr}")
+                if curr > prev:
+                    num_increase += 1
     
-    curr = sys.maxsize
-    prev = sys.maxsize
-    num_increase = 0
-    for sums in window_sums:
-        curr = sums
-        if prev != sys.maxsize and curr > prev:
-            num_increase += 1
-        prev = curr
+#     curr = sys.maxsize
+#     prev = sys.maxsize
+#     num_increase = 0
+#     for sums in window_sums
+#         curr = sums
+#         if prev != sys.maxsize and curr > prev:
+#             num_increase += 1
+#         prev = curr
     
     print(num_increase)
     return
