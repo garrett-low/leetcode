f68d1bfafe3eb5498cd3d6c9fbb4f49f1fb6d9cf
Garrett R Low <usernamegl@live.com>
2023-07-04 14:52:15 -0500

    dijkstra 2

--- python/fem_algos/graph/dijkstra.py
+++ python/fem_algos/graph/dijkstra.py
@@ -17,22 +17,22 @@ def dijkstra(graph):
             dist_dict[vertex] = sys.maxsize
             prev_dict[vertex] = None
         unvisited_min_heap.add(vertex, dist_dict[vertex])
-    print(f"starting distances: {dist_dict}")
-    print(f"starting nearest parent: {prev_dict}")
-    print(f"starting min heap: {unvisited_min_heap}")
+#     print(f"starting distances: {dist_dict}")
+#     print(f"starting nearest parent: {prev_dict}")
+#     print(f"starting min heap: {unvisited_min_heap}")
     
     while len(unvisited_min_heap) > 0:
         curr_element = unvisited_min_heap.remove()
         
         edge_list = graph.adj_list[curr_element]
-        print(f"visit: {curr_element}")
+#         print(f"visit: {curr_element}")
         for edge in edge_list:
             test_dist = dist_dict[curr_element] + edge.weight
             
             if test_dist < dist_dict[edge.to]:
                 dist_dict[edge.to] = test_dist
                 prev_dict[edge.to] = curr_element
-                print(f" * update shortest path --> {edge.to}: {test_dist}")
+#                 print(f" * update shortest path --> {edge.to}: {test_dist}")
                 unvisited_min_heap.update(edge.to, test_dist)
     
     return dist_dict, prev_dict
@@ -49,22 +49,22 @@ def dijkstra_path_to(graph, needle):
             dist_dict[vertex] = sys.maxsize
         prev_dict[vertex] = None
         unvisited_min_heap.add(vertex, dist_dict[vertex])
-    print(f"starting distances: {dist_dict}")
-    print(f"starting nearest parent: {prev_dict}")
-    print(f"starting min heap: {unvisited_min_heap}")
+#     print(f"starting distances: {dist_dict}")
+#     print(f"starting nearest parent: {prev_dict}")
+#     print(f"starting min heap: {unvisited_min_heap}")
     
     while len(unvisited_min_heap) > 0:
         curr_element = unvisited_min_heap.remove()
         
         edge_list = graph.adj_list[curr_element]
-        print(f"visit: {curr_element}")
+#         print(f"visit: {curr_element}")
         for edge in edge_list:
             test_dist = dist_dict[curr_element] + edge.weight
             
             if test_dist < dist_dict[edge.to]:
                 dist_dict[edge.to] = test_dist
                 prev_dict[edge.to] = curr_element
-                print(f" * update shortest path --> {edge.to}: {test_dist}")
+#                 print(f" * update shortest path --> {edge.to}: {test_dist}")
                 unvisited_min_heap.update(edge.to, test_dist)
     
     out_path = deque([needle])
---
+++ python/fem_algos/graph/dijkstra_2.py
@@ -0,0 +1,55 @@
+from collections import deque
+import sys
+from adj_list_weighted import adj_list
+from min_heap_node import min_heap
+
+# class edge:
+#     def __init__(self, to, weight):
+#         self.to = to
+#         self.weight = weight
+#     def __str__(self):
+#         return f"[{self.to}: {self.weight}]"
+
+def dijkstra(filename, to_node = None):
+    unvisited_min_heap = min_heap()
+    dist_dict = {}
+    prev_dict = {}
+    
+    graph = adj_list(filename)
+        
+    dist_dict['A'] = 0
+    for node in graph.adj_list.keys():
+        if node != 'A':
+            dist_dict[node] = sys.maxsize
+        prev_dict[node] = None
+        unvisited_min_heap.add(node, dist_dict[node])
+    
+    while not unvisited_min_heap.isEmpty():
+        curr_node = unvisited_min_heap.remove()
+        for neighbor in graph.adj_list[curr_node]:
+            old_dist = dist_dict[neighbor.to]
+            test_dist = dist_dict[curr_node] + neighbor.weight
+            if old_dist > test_dist:
+                prev_dict[neighbor.to] = curr_node
+                dist_dict[neighbor.to] = test_dist
+    
+    if to_node == None:
+        print(prev_dict)
+        print(dist_dict)
+    else:
+        curr_node = prev_dict[to_node]
+        output = deque(to_node)
+        while curr_node != None:
+            output.appendleft(curr_node)
+            curr_node = prev_dict[curr_node]
+        print(output)
+    return
+    
+def dict_syntax():
+    dict1 = {}
+    dict1.update({'A': 'Z'})
+    dict1.update({'B': 'Y'})
+    print(dict1)
+    
+dijkstra('input_list_weighted.txt')
+dijkstra('input_list_weighted.txt', 'E')
