65afe87e7b97214594b633ee504389cb55e2db5f
Garrett R Low <usernamegl@live.com>
2023-07-07 19:25:14 -0500

    quick sort

---
+++ python/fem_algos/sort/quick_sort_20230707.py
@@ -0,0 +1,34 @@
+def qs(arr):
+    qs_inner(arr, 0, len(arr) - 1)
+    print(arr)
+    return
+
+def qs_inner(arr, lo, hi):
+    if lo >= hi or lo < 0:
+        return
+    
+    pivot = partition(arr, lo, hi)
+    
+    qs_inner(arr, lo, pivot - 1)
+    qs_inner(arr, pivot + 1, hi)
+
+def partition(arr, lo, hi):
+    pivot = arr[hi]
+    
+    new_pivot = lo - 1
+    for i in range(lo, hi):
+        if arr[i] < pivot:
+            new_pivot += 1
+            temp = arr[i]
+            arr[i] = arr[new_pivot]
+            arr[new_pivot] = temp
+    
+    new_pivot += 1
+    arr[hi] = arr[new_pivot]
+    arr[new_pivot] = pivot
+    return new_pivot
+
+qs([420, 69, 350, 1234, 234, 34, 99, 5])
+
+# forgot return new pivot index
+# forgot base case
