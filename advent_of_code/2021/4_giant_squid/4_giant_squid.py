1285ee51d6f2433daca508269b3f89a018a5e198
Garrett R Low <usernamegl@live.com>
2023-07-07 19:16:03 -0500

    aoc 2021 day 4 p1

--- advent_of_code/2021/4_giant_squid/4_giant_squid.py
+++ advent_of_code/2021/4_giant_squid/4_giant_squid.py
@@ -3,7 +3,7 @@ def bingo(filename):
     drawn_num_list = []
     with open(filename, 'rt', encoding='utf-8') as file:
         first_line = file.readline().strip()
-        drawn_num_list = first_line.split(',')
+        drawn_num_list = [int(num) for num in first_line.split(',')]
         file.readline() # skip empty blank second line
         
         board_i = 0
@@ -15,16 +15,86 @@ def bingo(filename):
                 continue
             
             line = line.strip()
-            split = line.split(' ')
+            split = [int(num) for num in line.split()]
             board_array[board_i].append(split)
-      
-    print(drawn_num_list)
-    for board in board_array:
-        for row in board:
-            for col in board:
-                print(f"{col} ")
-            print()
-        print()
-        print()
     
+    # debug
+#     print(drawn_num_list)
+#     for board in board_array:
+#         for row in board:
+#             print(f"{row}")
+#         print()
+    
+    done = False
+    for drawn_num in drawn_num_list:
+        for board in board_array:
+            for row_num in range(len(board)):
+                for col_num in range(len(board[row_num])):
+                    if board[row_num][col_num] == drawn_num:
+                        board[row_num][col_num] = True
+                        done = check_bingo(board, row_num, col_num)
+                        if done == True:
+                            print_output(board, drawn_num)
+                            return
+    # debug
+#     print(drawn_num_list)
+#     for board in board_array:
+#         for row in board:
+#             print(f"{row}")
+#         print()
+
+diag_one = {(0,0), (1,1), (2,2), (3,3), (4,4)}
+diag_two = {(0,4), (1,3), (2,2), (3,1), (4,0)}
+def check_bingo(board, row_num, col_num):    
+    test_coord = (row_num, col_num)
+    if test_coord in diag_one:
+        is_bingo = True
+        for diag_coord in diag_one:
+            row, col = diag_coord
+            if board[row][col] != True:
+                is_bingo = False
+                break
+        if is_bingo == True:
+            return is_bingo
+    elif test_coord in diag_two:
+        is_bingo = True
+        for diag_coord in diag_one:
+            row, col = diag_coord
+            if board[row][col] != True:
+                is_bingo = False
+                break
+        if is_bingo == True:
+            return is_bingo
+    
+    # check horizontal
+    is_bingo = True
+    for col in range(0, 5):
+        if board[row_num][col] != True:
+            is_bingo = False
+            break
+    if is_bingo == True:
+        return is_bingo
+    # check vertical
+    is_bingo = True
+    for row in range(0, 5):
+        if board[row][col_num] != True:
+            is_bingo = False
+            break
+    if is_bingo == True:
+        return is_bingo
+    
+    return False
+
+def print_output(board, drawn_num):
+    unmarked_sum = 0
+    for row in range(0, 5):
+        for col in range(0, 5):
+            val = board[row][col]
+            if val != True:
+                unmarked_sum += val
+    
+    print(f"{unmarked_sum * drawn_num}")
+    return unmarked_sum * drawn_num
+
 bingo('sample.txt')
+bingo('input.txt')
