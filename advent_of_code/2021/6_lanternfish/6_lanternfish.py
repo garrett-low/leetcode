5aa39e75c204b48954b22960bcb48e8371487897
Garrett R Low <usernamegl@live.com>
2023-07-11 22:52:47 -0700

    aoc 2021 day6 p2

--- advent_of_code/2021/6_lanternfish/6_lanternfish.py
+++ advent_of_code/2021/6_lanternfish/6_lanternfish.py
@@ -6,7 +6,7 @@ def spawn(filename, num_days):
         input_str = '[' + file.readline().strip() + ']'
         ary = ast.literal_eval(input_str)
     
-    print(ary)
+#     print(ary)
     for _ in range(num_days):
         start_len = len(ary) # don't decrement new fish
         for i in range(start_len):
@@ -19,5 +19,45 @@ def spawn(filename, num_days):
     
     print(len(ary))
 
-spawn('sample.txt', 1)
+# spawn('sample.txt', 80)
 # spawn('input.txt', 80)
+
+def spawn2(filename, num_days):
+    ary = []
+    fish_dict = {}
+    for i in range(9):
+        fish_dict[i] = 0
+    
+    with open(filename, 'rt', encoding='utf-8') as file:
+        while True:
+            char = file.read(1)
+            if not char:
+                break
+            elif char == ',':
+                continue
+            else:
+                fish_dict[int(char)] += 1
+    
+#     print(fish_dict)
+    for day_num in range(num_days):
+        zero_fish = fish_dict[0]
+        seven_fish = fish_dict[7]
+        for fish_idx in range(8):
+            if fish_idx == 6:
+                continue
+            fish_dict[fish_idx] = fish_dict[fish_idx + 1]
+        fish_dict[8] = zero_fish
+        fish_dict[6] = zero_fish + seven_fish
+
+#         print(f"day {day_num}: {fish_dict}")
+    
+    ret_sum = 0
+    for key in fish_dict:
+        ret_sum += fish_dict[key]
+    
+    print(ret_sum)
+
+spawn2('sample.txt', 80)
+spawn2('input.txt', 80)
+spawn2('sample.txt', 256)
+spawn2('input.txt', 256)
