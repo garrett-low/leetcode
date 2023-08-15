a9637faada8033e1c48834d70661e5b0c74ebb0c
Garrett R Low <usernamegl@live.com>
2023-08-14 22:42:54 -0500

    dynamic programming

--- python/fem_algos/dynamic_programming/add_until_100_dp.py
+++ python/fem_algos/dynamic_programming/add_until_100_dp.py
@@ -4,7 +4,35 @@
 # ignored. However, this function makes unnecessary recursive calls. Fix
 # the code to eliminate the unnecessary recursion:
 
-def add_until_100(arr):
-    if len(arr) == 0:
+# I would never write it this way in the first place, but this is how the book
+# wrote it as an example.
+# It also adds the array in reverse (last index first)...?
+def add_100_non_dp(arr):
+    output = add_until_100_non_dp(arr, 0)
+    print(output)
+
+def add_until_100_non_dp(arr, i):
+    if i >= len(arr):
         return 0
+    if arr[i] + add_until_100_non_dp(arr, i + 1) > 100:
+        return add_until_100_non_dp(arr, i + 1)
+    else:
+        return arr[i] + add_until_100_non_dp(arr, i + 1)
+
+add_100_non_dp([30, 33, 28, 29, 27, 26])
+
+def add_100(arr):
+    output = add_100_inner(arr, 0, 0)
+    print(output)
+
+def add_100_inner(arr, i, curr_sum):
+    if i >= len(arr):
+        return curr_sum
     
+    if curr_sum + arr[i] < 100:
+        curr_sum += arr[i]
+    
+    curr_sum = add_100_inner(arr, i + 1, curr_sum)
+    return curr_sum
+    
+add_100([30, 33, 28, 29, 27, 26])
