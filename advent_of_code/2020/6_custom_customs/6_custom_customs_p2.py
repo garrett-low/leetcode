34f2af52bbb3bcb4f9e73f68ff5925b8951af595
Garrett R Low <usernamegl@live.com>
2023-08-23 23:56:29 -0500

    aoc2020 day6 p2

---
+++ advent_of_code/2020/6_custom_customs/6_custom_customs_p2.py
@@ -0,0 +1,51 @@
+def count_answers(filename):
+    yes_count = 0
+    with open(filename, 'rt', encoding = 'utf-8') as file:
+        curr_group_answer_list = []
+        for line in file:
+            if line == '\n':
+                # print(curr_group_answer_list)
+                count_answer = check_same_answer(curr_group_answer_list)
+                # print(count_answer)
+                yes_count += count_answer
+                curr_group_answer_list = []
+                continue
+            
+            person_answer_set = set(line.strip())
+            curr_group_answer_list.append(person_answer_set)
+        
+        yes_count += check_same_answer(curr_group_answer_list)
+    print(yes_count)
+
+def check_same_answer(curr_group_answer_list):
+    if len(curr_group_answer_list) <= 0:
+        return 0
+    
+    first_person_answer_set = curr_group_answer_list[0]
+    if len(curr_group_answer_list) == 1:
+        return len(first_person_answer_set)
+    
+    for i in range(1, len(curr_group_answer_list)):
+        curr_answer_set = curr_group_answer_list[i]
+        set_intersect = set()
+        
+        if len(curr_answer_set) < len(first_person_answer_set):
+            smaller = curr_answer_set
+            larger = first_person_answer_set
+        else:
+            larger = curr_answer_set
+            smaller = first_person_answer_set
+        
+        for answer in smaller:
+            if answer in larger:
+                set_intersect.add(answer)
+        
+        if len(set_intersect) <= 0:
+            return 0
+        
+        first_person_answer_set = set_intersect
+    
+    return len(set_intersect)
+
+count_answers('sample2.txt')
+count_answers('input.txt') # p1 6735
