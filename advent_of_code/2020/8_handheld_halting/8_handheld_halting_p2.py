7ecefe52cfb8aa9cabc229608821c09ac33ff540
Garrett R Low <usernamegl@live.com>
2023-08-26 21:22:29 -0500

    aoc2020d8p2 - 1287 was too high** also 2517 too high

--- advent_of_code/2020/8_handheld_halting/8_handheld_halting_p2.py
+++ advent_of_code/2020/8_handheld_halting/8_handheld_halting_p2.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-def acc_before_infinite(filename):
+def handheld_halting_p2(filename):
     op_list = []
     with open(filename, 'rt', encoding = 'utf-8') as file:
         for line in file:
@@ -9,83 +9,64 @@ def acc_before_infinite(filename):
             val = int(line_split[1])
             op_list.append((op, val))
     
-    acc = 0
-    i = 0
-    line_exec_ct = defaultdict(int)
-    # P2
-    op_list_with_prev = {}
-    last_op_i = None
-    prev = (op_list[0][0], op_list[0][1], i)
-    while i < len(op_list):
-        op, val = op_list[i]
-        op_list_with_prev[i] = prev # p2
-        
-        line_exec_ct[i] += 1
-        if line_exec_ct[i] >= 2:
-            last_op_i = i # p2
-            break
-        
-        prev = (op, val, i) # p2
-        if op == 'acc':
-            acc += val
-            i += 1
-        elif op == 'jmp':
-            i += val
-        elif op == 'nop':
-            i += 1
+    visited_set = set()
+    path = []
+    is_finished, accumulator = exec_with_swap(op_list, visited_set, path, 0, 0)
+    print(accumulator)
     
-    print(f"P1 acc before first repeat: {acc}")
-    
-    max_i = 0
-    seen = set()
-    while last_op_i and last_op_i not in seen:
-        seen.add(last_op_i)
-        op, val, prev_i = op_list_with_prev[last_op_i]
-        # print(op, val, prev)
-        if prev_i > max_i:
-            max_i = prev_i
-        last_op_i = prev_i
-    # print(max_i)
-    tmp_op, tmp_val = op_list[max_i]
-    
-    if tmp_op == 'jmp':
-        tmp_op = 'nop'
-    elif tmp_op == 'nop':
-        tmp_op = 'jmp'
-    
-    op_list[max_i] = (tmp_op, tmp_val)
-    # print((tmp_op, tmp_val))
-    
-    #p2 redo
-    acc = 0
-    i = 0
-    line_exec_ct = defaultdict(int)
-    while i < len(op_list):
-        op, val = op_list[i]
-        # print(op, val)
-        
-        # line_exec_ct[i] += 1
-        # if line_exec_ct[i] >= 2:
-            # break
-        
-        if op == 'acc':
-            acc += val
-            i += 1
-        elif op == 'jmp':
-            i += val
-        elif op == 'nop':
-            i += 1
-    
-    print("P2: ", acc)
-    
-acc_before_infinite('sample.txt')
-# acc_before_infinite('input.txt')
+    # debug
+    for tuple in path:
+        step_i, op, val, accumulator = tuple
+        print(f"{step_i}:\t{op}\t{val}\t{accumulator}")
 
-# p2 first attempt 1287
+def exec_with_swap(op_list, visited_set, path, step_i, accumulator):
+    if step_i == len(op_list):
+        # print(f"Fin:\t\t\t{accumulator}")
+        path.append((step_i, None, None, accumulator))
+        return True, accumulator
+    
+    if step_i in visited_set:
+        return False, 0
+    
+    visited_set.add(step_i)
+    op, val = op_list[step_i]
+    # print(f"{step_i}:\t{op}\t{val}\t{accumulator}")
+    path.append((step_i, op, val, accumulator))
+    new_step_i, new_accumulator = exec_op(op, val, step_i, accumulator)
+    is_finished, final_accumulator = exec_with_swap(op_list, visited_set, path, new_step_i, new_accumulator)
+    if is_finished:
+        return True, final_accumulator
+    path.pop()
+    
+    # didn't return True, try swap
+    if op == 'nop' or op == 'jmp':
+        if op == 'nop':
+            op = 'jmp'
+        else:
+            op = 'nop'
+        
+        path.append((step_i, op, val, accumulator))
+        new_step_i, new_accumulator = exec_op(op, val, step_i, accumulator)
+        is_finished, final_accumulator = exec_with_swap(op_list, visited_set, path, new_step_i, new_accumulator)
+        if is_finished:
+            return True, final_accumulator
+        path.pop()
+    
+    return False, 0
+    
+def exec_op(op, val, step_i, accumulator):     
+    if op == 'acc':
+        accumulator += val
+        step_i += 1
+    elif op == 'jmp':
+        step_i += val
+    elif op == 'nop':
+        step_i += 1
+    
+    return step_i, accumulator
 
-# def test():
-    # print(int('+4'))
-    # print(int('-4'))
-    # test_dict = defaultdict(int)
-    # print(test_dict[0])
-# test()
+# handheld_halting_p2('sample.txt')
+handheld_halting_p2('input.txt')
+
+# p2 1287 too high
+# 2512 too high
