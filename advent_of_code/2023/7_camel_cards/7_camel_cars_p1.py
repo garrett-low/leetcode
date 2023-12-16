4ab0b2d1a815fb271cab7f790086302a7ceef197
Garrett R Low <usernamegl@live.com>
2023-12-15 22:47:58 -0600

    aoc2023d7p1 - fin?

--- advent_of_code/2023/7_camel_cards/7_camel_cars_p1.py
+++ advent_of_code/2023/7_camel_cards/7_camel_cars_p1.py
@@ -1,20 +1,22 @@
 from collections import defaultdict
 from functools import cmp_to_key
+from collections import namedtuple
 
 HAND_TYPE = {
-    "five_kind" : 1,
-    "four_kind" : 2,
-    "full_house" : 3,
+    "five_kind" : 7,
+    "four_kind" : 6,
+    "full_house" : 5,
     "three_kind" : 4,
-    "two_pair" : 5,
-    "one_pair" : 6,
-    "high_card" : 7 }
+    "two_pair" : 3,
+    "one_pair" : 2,
+    "high_card" : 1 }
 
 FACE_VAL = {
     "A" : 1,
     "K" : 13,
     "Q" : 12,
-    "J" : 11
+    "J" : 11,
+    "T" : 10
 }
 
 def cards(filename):
@@ -26,8 +28,8 @@ def cards(filename):
             hands.append(splits[0])
             bids.append(int(splits[1]))
     
-    print(hands)
-    print(bids)
+    # print(hands)
+    # print(bids)
     
     hands_card_counts = [] # list of hands, each item is dict of card -> count
     hands_card_counts_reverse = [] # list of hands, count -> list of cards
@@ -79,26 +81,46 @@ def cards(filename):
         else:
             hands_hand_type.append("high_card")
     
-    print(hands_hand_type)
+    # I just learned about named tuples
+    hand_tuples = []
+    Hand = namedtuple('Hand', ['hand_string', 'hand_type', 'hand_bid'])
+    for i in range(len(hands)):
+        this_hand_string = hands[i]
+        this_hand_type = hands_hand_type[i]
+        this_hand_bid = bids[i]
+        this_hand_tuple = Hand(this_hand_string, this_hand_type, this_hand_bid)
+        hand_tuples.append(this_hand_tuple)
     
-    print(sorted(hands_hand_type, key=cmp_to_key(compare_hand_types)))
+    hand_tuples_sorted = sorted(hand_tuples, key=cmp_to_key(compare_hand_types))
+    score = 0
+    for i, hand_tuple in enumerate(hand_tuples_sorted):
+        # print(hand_tuple)
+        rank = i + 1
+        bid = hand_tuple.hand_bid
+        score += rank * bid
+        # print(f"{score} += {rank} * {bid}")
     
-    print(hands_hand_type)
+    print(score)
 
-
-def compare_hand_types(type1, type2):
+def compare_hand_types(hand_tuple1, hand_tuple2):
+    type1 = hand_tuple1.hand_type
+    type2 = hand_tuple2.hand_type
     rank1 = HAND_TYPE[type1]
     rank2 = HAND_TYPE[type2]
     
     if rank1 < rank2:
-        return 1
-    elif rank1 > rank2:
         return -1
+    elif rank1 > rank2:
+        return 1
     else:
-        return 0
+        # secondary sort
+        return compare_hand_ordering(hand_tuple1, hand_tuple2)
         
-def compare_hand_ordering(hand1, hand2):
-    for i in range(len(hand)):
+def compare_hand_ordering(hand_tuple1, hand_tuple2):
+    hand1 = hand_tuple1.hand_string
+    hand2 = hand_tuple2.hand_string
+    # print(f"  {hand1} vs. {hand2}")
+    for i in range(len(hand1)):
         card1 = hand1[i]
         card2 = hand2[i]
         
@@ -107,9 +129,15 @@ def compare_hand_ordering(hand1, hand2):
         if card2.isalpha():
             card2 = FACE_VAL[card2.upper()]
         
-        if card1 > card2:
+        card1_int = int(card1)
+        card2_int = int(card2)
+        
+        # print(f"  {hand1[i]}: {card1}: {card1_int}")
+        # print(f"  {hand2[i]}: {card2}: {card2_int}")
+        
+        if card1_int < card2_int:
             return -1
-        elif card2 < card1:
+        elif card1_int > card2_int:
             return 1
         else:
             continue # just making it obvious
@@ -117,3 +145,4 @@ def compare_hand_ordering(hand1, hand2):
     return 0
 
 cards('sample.txt')
+cards('input.txt') # 250721888
