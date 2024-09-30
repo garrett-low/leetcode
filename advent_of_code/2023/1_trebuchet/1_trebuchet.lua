40284b118aa7148435a254397bd6f3e0084b898c
Garrett R Low <usernamegl@live.com>
2024-09-30 00:21:50 -0500

    aoc2023d1p1 lua

--- advent_of_code/2023/1_trebuchet/1_trebuchet.lua
+++ advent_of_code/2023/1_trebuchet/1_trebuchet.lua
@@ -8,9 +8,47 @@ function trebuchet(file)
     end
     handle:close()
     
-    for key, val in ipairs(arr) do print(key, val) end
+    -- for key, val in ipairs(arr) do print(key, val) end
     
     return arr
 end
 
-trebuchet("input.txt")
+function part_one(file)
+    local vals = {}
+    local input_lines = trebuchet(file)
+    for i = 1, #input_lines, 1 do
+        local input_line = input_lines[i]
+        local first_dig = nil
+        local last_dig = nil
+        for j = 1, #input_line, 1 do
+            local char_curr = input_line:sub(j, j)
+            -- print(char_curr)
+            -- local char_type = type(char_curr)
+            if tonumber(char_curr) then
+                first_dig = char_curr
+                -- print(char_curr)
+                break
+            end
+        end
+        for j = #input_line, 1, -1 do
+            local char_curr = input_line:sub(j, j)
+            if tonumber(char_curr) then
+                last_dig = char_curr
+                break
+            end
+        end
+        table.insert(vals, tostring(first_dig) .. tostring(last_dig))
+    end
+    
+    for key, val in ipairs(vals) do print(key, val) end
+    local sum = 0
+    for key, val in ipairs(vals) do
+        sum = sum + tonumber(val)
+    end
+    
+    print("P1: " .. sum)
+    return sum
+end
+
+trebuchet("sample.txt")
+part_one("sample.txt")
