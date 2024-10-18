3b436259e73e7e688d8e64d8445cca4db33fb8bf
Garrett R Low <usernamegl@live.com>
2024-10-17 18:30:20 -0700

    lua aoc2020d1p2 finished

--- advent_of_code/2020/1_report_repair/1_report_repair.lua
+++ advent_of_code/2020/1_report_repair/1_report_repair.lua
@@ -35,3 +35,69 @@ end
 
 report_repair("sample.txt")
 report_repair("input.txt")
+
+function report_repair_p2(file)
+	local arr = {}
+	local handle = assert(io.open(file, "r"))
+	local value = handle:read("l")
+	while value do
+		arr[tonumber(value)] = true
+		value = handle:read("l")
+	end
+	handle:close()
+
+	-- for key, val in pairs(arr) do
+	-- 	print(key, val)
+	-- end
+
+	-- return arr
+	-- local second_val = -1
+	-- local curr_val = -1
+	local first_remainder = -1
+	local second_remainder = -1
+	local is_found = false
+	local p2_sol = -1
+	for key, _ in pairs(arr) do
+		-- curr_val = key
+		-- first_remainder = TARGET_SUM - curr_val
+		first_remainder = TARGET_SUM - key
+		-- print("P2 first val: " .. key)
+		-- print("P2 first remainder: " .. first_remainder)
+
+		for second_key, _ in pairs(arr) do
+			if key == second_key then
+				goto continue
+			end
+
+			second_remainder = first_remainder - second_key
+			if second_remainder < 0 then
+				goto continue
+			end
+			-- print("  P2 second val: " .. second_key)
+			-- print("  P2 second remainder: " .. second_remainder)
+			-- Forgot to convert the input
+			if arr[second_remainder] then
+				-- print("P2 found!")
+				is_found = true
+				p2_sol = key * second_key * second_remainder
+				break
+			end
+
+			::continue::
+		end
+
+		if is_found then
+			break
+		end
+	end
+
+	if is_found then
+		-- local p2_sol = key * second_key * second_remainder
+		print("P2: " .. p2_sol)
+	else
+		print("P2 not found!")
+	end
+end
+
+report_repair_p2("sample.txt")
+report_repair_p2("input.txt")
