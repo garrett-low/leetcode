5719c85b72c4b294ea15370ca9e9d6bec641b70a
Garrett R Low <usernamegl@live.com>
2024-12-03 01:22:30 -0600

    Finished AOC 2024 d1p1 in CPP

--- advent_of_code/2024/1_historian_hysteria/1_historian_histeria.cpp
+++ advent_of_code/2024/1_historian_hysteria/1_historian_histeria.cpp
@@ -2,30 +2,83 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
 
-int main() 
+int main(int argc, char** argv)
 {
-    // using namespace std;
+    using namespace std;
     
-    std::ifstream ifs("sample.txt");
+    if (argc != 2)
+    {
+        perror("Specify an input file!");
+        return 1;
+    }
+    
+    string filename = argv[1];    
+    
+    ifstream ifs(filename);
     if (!ifs)
     {
         perror("couldn't open");
         return 1;
     }
     
-    std::vector<std::string> list1;
-    std::vector<std::string> list2;
+    vector<int> list1;
+    vector<int> list2;
     
-    for (std::string line; std::getline(ifs, line); )
+    for (string line; getline(ifs, line); )
     {
-        list1.push_back(line);
+        istringstream stream(line);
+        vector<int> nums;
+        
+        for (string num; stream >> num; )
+        {
+            try 
+            {
+                nums.push_back(stoi(num));
+            } 
+            catch (const std::invalid_argument& e) 
+            {
+                std::cerr << "Invalid number format: " << line << std::endl;
+            } 
+            catch (const std::out_of_range& e) 
+            {
+                std::cerr << "Number out of range: " << line << std::endl;
+            }
+        }
+        
+        if (nums.size() != 2)
+        {
+            perror("Line didn't have two numbers on it!");
+            return 1;
+        }
+        
+        list1.push_back(nums[0]);
+        list2.push_back(nums[1]);
     }
     
-    for (int i = 0; i < list1.size(); i++) 
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
+    
+    // for (int i = 0; i < list1.size(); i++) 
+    // {
+        // cout << list1[i] << '\n';
+    // }
+
+    // for (int i = 0; i < list2.size(); i++) 
+    // {
+        // cout << list2[i] << '\n';
+    // }
+    
+    int sum;
+    for (int i = 0; i < list1.size(); i++)
     {
-        std::cout << list1[i] << '\n';
+        sum += abs(list1[i] - list2[i]);
     }
+    
+    cout << "P1: " << sum << '\n';
     
     return 0;
 }
