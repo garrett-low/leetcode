e3d7c065a97882d6b471282750f71cfee000c9e0
Garrett R Low <usernamegl@live.com>
2024-12-20 00:27:28 -0600

    Parse single line file

--- advent_of_code/2024/3_mull_it_over/3_mull_it_over.cpp
+++ advent_of_code/2024/3_mull_it_over/3_mull_it_over.cpp
@@ -1,42 +1,97 @@
-#include <iostream>
-#include <string>
-#include <string_view>
-#include <vector>
-#include <fstream>
-#include <sstream>
+#include <algorithm>
+#include <cctype>
 #include <cmath>
 #include <format>
-#include <unordered_set>
+#include <fstream>
+#include <iostream>
 #include <memory>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_set>
+#include <vector>
 
 void read_file(std::string_view filename, std::string &input);
 
-int main(int argc, char** argv)
-{
-	if (argc != 2)
-	{
-		perror("Specify an input file!");
-		return 1;
-	}
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    perror("Specify an input file!");
+    return 1;
+  }
 
-	std::string filename = argv[1];
+  std::string filename = argv[1];
 
-	std::string input{};
-	read_file(filename, input);
-	std::cout << input << '\n';
+  std::string input{};
+  read_file(filename, input);
+  // std::cout << input << '\n';
 
+  int score{};
 
+  for (int i = 0; i < input.length(); i++) {
+    std::string op{input.substr(i, 3)};
+    // std::cout << op << '\n';
+    if (op != "mul") {
+      continue;
+    }
+    // std::cout << op << '\n';
+
+    i = i + 3;
+    char curr = input[i];
+    if (curr != '(') {
+      continue;
+    }
+    curr = input[++i];
+    std::string first_operand{};
+    while (std::isdigit(curr) && i < input.length()) {
+      first_operand += curr;
+      curr = input[++i];
+    }
+    // std::cout << first_operand << '\n';
+
+    if (first_operand.empty() ||
+        !std::all_of(first_operand.begin(), first_operand.end(), ::isdigit)) {
+      continue;
+    }
+
+    // std::cout << "after first_op: " << curr << '\n';
+    if (curr != ',') {
+      continue;
+    }
+
+    std::string sec_operand{};
+    curr = input[++i];
+    while (std::isdigit(curr) && i < input.length()) {
+      sec_operand += curr;
+      curr = input[++i];
+    }
+    // std::cout << sec_operand << '\n';
+
+    if (sec_operand.empty() ||
+        !std::all_of(sec_operand.begin(), sec_operand.end(), ::isdigit)) {
+      continue;
+    }
+
+    if (curr != ')') {
+      continue;
+    }
+
+    int first_op = std::stoi(first_operand);
+    int sec_op = std::stoi(sec_operand);
+
+    // std::cout << "first_op: " << first_op << '\n' << ", second op: " << sec_op << '\n';
+    score += (first_op * sec_op);
+  }
+  // std::cout << '\n';
+  std::cout << "P1: " << score << '\n';
 }
 
-void read_file(std::string_view filename, std::string &input)
-{
-	std::string file{filename};
-	std::ifstream ifs(file);
-	if (!ifs)
-	{
-		perror("Couldn't open file!");
-		return;
-	}
+void read_file(std::string_view filename, std::string &input) {
+  std::string file{filename};
+  std::ifstream ifs(file);
+  if (!ifs) {
+    perror("Couldn't open file!");
+    return;
+  }
 
-	std::getline(ifs, input);
+  std::getline(ifs, input);
 }
