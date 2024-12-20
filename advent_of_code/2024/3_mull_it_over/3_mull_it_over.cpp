fb9f22517a0e4165dcbdb3311cbe288e1529b4fb
Garrett R Low <usernamegl@live.com>
2024-12-19 23:19:06 -0600

    Start AOC2024D3P1

--- advent_of_code/2024/3_mull_it_over/3_mull_it_over.cpp
+++ advent_of_code/2024/3_mull_it_over/3_mull_it_over.cpp
@@ -11,8 +11,6 @@
 
 void read_file(std::string_view filename, std::string &input);
 
-int parse(std::string_view input, std::unique_ptr<int> index);
-
 int main(int argc, char** argv)
 {
 	if (argc != 2)
@@ -27,27 +25,7 @@ int main(int argc, char** argv)
 	read_file(filename, input);
 	std::cout << input << '\n';
 
-	int* index = 0;
-	std::unique_ptr<int> index_ptr{new int};
-	*index_ptr = 1234;
-	std::cout << &index_ptr << '\n';
-	std::cout << *index_ptr << '\n';
 
-
-	parse(input, index_ptr);
-}
-
-int parse(std::string_view input, std::unique_ptr<int> index) {
-	int r = *index + 3;
-	std::cout << "right index: " << r << '\n';
-	if (r > input.length() - 1) {
-		return 0;
-	}
-	std::string input_str{input};
-	std::string op = input_str.substr(*index, *index + 3);
-
-	std::cout << "op: " << op << '\n';
-	return 1234;
 }
 
 void read_file(std::string_view filename, std::string &input)
